69e4c5682bc3c31587827be0ed365be0d91231fe
Your Name <you@example.com>
2024-02-19 18:06:02 +0500

    Сделал базовую логику

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
+btn__add = document.querySelector('.add')
+container = document.querySelector('div')
+input = document.querySelector('input')
+
+const create__list = () => {
+    let p = document.createElement('p')
+    p.classList.add('item')
+    p.textContent = input.value
+    container.appendChild(p)
+}
+
+btn__add.addEventListener('click', create__list)
 
