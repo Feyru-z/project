6e33bb4c18a861592da6de95225d2e4930cfbb1d
ImZloyopDev <imzloyopdev@gmail.com>
2024-02-25 18:28:03 +0500

    13

--- code.js
+++ code.js
@@ -3,20 +3,26 @@ let container = document.querySelector('div')
 let todo = document.querySelector('ul')
 let input = document.querySelector('input')
 let template = document.querySelector('#element').content
-let btn__rem = document.querySelector('.rem')
+
 
 const create__list = () => {
     let clone = template.cloneNode(true)  
     let li = clone.querySelector('.item')
+    let btn__rem = clone.querySelector('.rem')
     li.textContent = li
     li.textContent = input.value
     todo.append(li)
+    todo.append(btn__rem)
     input.value = ''
     localST('', li)
 }
 
-const remove__list = (n, item) => {
+const remove__LS = (n, item) => {
     localStorage.removeItem(n, item)
+}
+
+const remove__list = (item) => {
+    item.remove()
 }
 
 btn__add.addEventListener('click', create__list)
