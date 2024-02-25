e10eb2fc2f3bd0b1bb9e85ca631fda067aa31ea7
Feyru-z <zhirafik2003@gmail.com>
2024-02-25 15:51:50 +0300

    9

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ let container = document.querySelector('div')
 let todo = document.querySelector('ul')
 let input = document.querySelector('input')
 let template = document.querySelector('#element').content
+let btn__rem = document.querySelector('.rem')
 
 const create__list = () => {
     let clone = template.cloneNode(true)  
@@ -11,7 +12,16 @@ const create__list = () => {
     li.textContent = input.value
     todo.append(li)
     input.value = ''
+    localST('', li)
+}
+
+const remove__list = (n, item) => {
+    localStorage.removeItem(n, item)
 }
 
 btn__add.addEventListener('click', create__list)
+btn__rem.addEventListener('click', remove__list)
 
+function localST(n, item) {
+    localStorage.setItem(n, JSON.stringify(item));
+}
