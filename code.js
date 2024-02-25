923276f36e7816593af2a28f982a16190affc9e7
ImZloyopDev <imzloyopdev@gmail.com>
2024-02-25 17:55:12 +0500

    10

--- code.js
+++ code.js
@@ -1,13 +1,27 @@
-btn__add = document.querySelector('.add')
-container = document.querySelector('div')
-input = document.querySelector('input')
+let btn__add = document.querySelector('.add')
+let container = document.querySelector('div')
+let todo = document.querySelector('ul')
+let input = document.querySelector('input')
+let template = document.querySelector('#element').content
+let btn__rem = document.querySelector('.rem')
 
 const create__list = () => {
-    let p = document.createElement('p') // заменить на template
-    p.classList.add('item')
-    p.textContent = input.value
-    container.appendChild(p)
+    let clone = template.cloneNode(true)  
+    let li = clone.querySelector('.item')
+    li.textContent = li
+    li.textContent = input.value
+    todo.append(li)
+    input.value = ''
+    localST('', li)
+}
+
+const remove__list = (n, item) => {
+    localStorage.removeItem(n, item)
 }
 
 btn__add.addEventListener('click', create__list)
+btn__rem.addEventListener('click', remove__list)
 
+function localST(n, item) {
+    localStorage.setItem(n, JSON.stringify(item));
+}
